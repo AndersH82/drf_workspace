17cee3676bc07f3c40122c8dc893cf7d3822758a
Anders Holst <andersthereseholst@gmail.com>
2024-03-16 19:07:54 +0000

    add login/out view, permissions, ProfileSerializer

--- profiles/views.py
+++ profiles/views.py
@@ -4,33 +4,47 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializers
+from api.permissions import IsOwnerOrReadOnly
 
 
 class ProfileList(APIView):
+    """
+    List all profiles
+    No Create view (post method), as profile creation handled by django signals
+    """
     def get(self, request):
         profiles = Profile.objects.all()
-        serializer = ProfileSerializers(profiles, many=True)
+        serializer = ProfileSerializers(
+            profiles, many=True, context={'request': request}
+        )
         return Response(serializer.data)
 
 
 class ProfileDetail(APIView):
     serializer_class = ProfileSerializers
+    permission_classes = [IsOwnerOrReadOnly]
+
     def get_object(self, pk):
         try:
             profile = Profile.objects.get(pk=pk)
+            self.check_object_permissions(self.request, profile)
             return profile
         except Profile.DoesNotExist:
             raise Http404
 
     def get(self, request, pk):
         profile = self.get_object(pk)
-        serializer = ProfileSerializers(profile)
+        serializer = ProfileSerializers(
+            profile, context={'request': request}
+        )
         return Response(serializer.data)
 
     def put(self, request, pk):
         profile = self.get_object(pk)
-        serializer = ProfileSerializers(profile, data=request.data)
+        serializer = ProfileSerializers(
+            profile, data=request.data, context={'request': request}
+        )
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
