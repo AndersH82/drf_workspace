a3a172a583cc17ebb2aa4ab9c492899d5589de9a
Anders Holst <andersthereseholst@gmail.com>
2024-03-21 15:00:58 +0000

    tokens

--- api/settings.py
+++ api/settings.py
@@ -16,10 +16,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [(
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication'
         if 'DEV' in os.environ
-        else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+        else 'rest_framework_simplejwt.authentication.JWTAuthentication'
     )],
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
