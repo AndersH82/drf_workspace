60147aeda9842d6cd07cae80f528323958f1e993
Anders Holst <andersthereseholst@gmail.com>
2024-03-21 10:33:43 +0000

    cors settings

--- api/settings.py
+++ api/settings.py
@@ -88,9 +88,9 @@ INSTALLED_APPS = [
 SITE_ID = 1
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
