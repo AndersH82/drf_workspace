44376fbf5a33d54d10447e72134c7aecc2794a11
Anders Holst <andersthereseholst@gmail.com>
2024-03-22 14:40:16 +0000

    setiings fix

--- api/settings.py
+++ api/settings.py
@@ -147,7 +147,7 @@ if 'DEV' in os.environ:
     }
 else:
     DATABASES = {
-        'default': dj_database_url.config(os.environ.get("DATABASE_URL"))
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
 
 
